42e2f9acfbe6ff07cb883ae17d7fd0e037c6bc45
Bob <bob@hoverdog.com>
2015-02-23 21:42:00 -0600

    arms work with control panel

--- src/Main.java
+++ src/Main.java
@@ -1,128 +1,203 @@
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.*;
-import javax.swing.border.Border;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 
-public class Main {
-	
-	private static JButton inc_ax1, dec_ax1, inc_ax2, dec_ax2, inc_ax3, dec_ax3;
-	private static JLabel deg_ax1, deg_ax2, deg_ax3;
-	
-	private static class RobotDisplay extends JPanel {
-		public void paintComponent(Graphics g) {
-			super.paintComponent(g);
-			//g.drawString("Robot Area", 20, 10);
-			//g.drawString("Control Panel", 700, 10);
-			g.setColor(Color.BLACK);
-			g.fillRect(0, 0, 600, 600);
+public class Main
+{
+
+	/**
+	 * All the buttons to interact with the robotic arm
+	 */
+	private static JButton incAx1, decAx1, incAx2, decAx2, incAx3, decAx3;
+
+	/**
+	 * Displays the current angles of the robotic arm
+	 */
+	private static JLabel degAx1, degAx2, degAx3;
+
+	/**
+	 * manages each of the three robotic arm segments
+	 */
+	private static RoboticArm roboticArm;
+
+	/**
+	 * takes care of drawing the robotic arm
+	 */
+	private static RobotDisplay displayPanel;
+
+	private static class RobotDisplay extends JPanel
+	{
+		public void paintComponent( Graphics g )
+		{
+			super.paintComponent( g );
+			g.setColor( Color.BLACK );
+			g.fillRect( 0, 0, 600, 600 );
+			roboticArm.onDraw( g );
 		}
 	}
 
-	private static class OnAxisButtonPress implements ActionListener {
-		public void actionPerformed(ActionEvent e) {
-			
+	/**
+	 * Called whenever a button is pressed
+	 */
+	private static class OnAxisButtonPress implements ActionListener
+	{
+		public void actionPerformed( ActionEvent e )
+		{
+			JButton button = (JButton) e.getSource();
+			if ( button.equals( incAx1 ) )
+			{
+				roboticArm.incrementLink1();
+				degAx1.setText( "" + roboticArm.getLink1Angle() );
+			}
+			if ( button.equals( incAx2 ) )
+			{
+				roboticArm.incrementLink2();
+				degAx2.setText( "" + roboticArm.getLink2Angle() );
+			}
+			if ( button.equals( incAx3 ) )
+			{
+				roboticArm.incrementLink3();
+				degAx3.setText( "" + roboticArm.getLink3Angle() );
+			}
+			if ( button.equals( decAx1 ) )
+			{
+				roboticArm.decrementLink1();
+				degAx1.setText( "" + roboticArm.getLink1Angle() );
+			}
+			if ( button.equals( decAx2 ) )
+			{
+				roboticArm.decrementLink2();
+				degAx2.setText( "" + roboticArm.getLink2Angle() );
+			}
+			if ( button.equals( decAx3 ) )
+			{
+				roboticArm.decrementLink3();
+				degAx3.setText( "" + roboticArm.getLink3Angle() );
+			}
+			roboticArm.onTranslate();
+			displayPanel.repaint();
 		}
 	}
 
-	public static void main(String[] args) {
+	public static void main( String[] args )
+	{
 
-		RobotDisplay displayPanel = new RobotDisplay();
-		displayPanel.setBounds(0, 0, 600, 600);
-		
+		roboticArm = new RoboticArm( 300, 600 );
+
+		displayPanel = new RobotDisplay();
+		displayPanel.setBounds( 0, 0, 600, 600 );
+
 		OnAxisButtonPress onAxisButtonPress = new OnAxisButtonPress();
-		
+
 		/**
 		 * Create axis editing buttons
 		 */
-		inc_ax1 = new JButton("+");
-		inc_ax1.addActionListener(onAxisButtonPress);
-		dec_ax1 = new JButton("-");
-		dec_ax1.addActionListener(onAxisButtonPress);
-		
-		inc_ax2 = new JButton("+");
-		inc_ax2.addActionListener(onAxisButtonPress);
-		dec_ax2 = new JButton("-");
-		dec_ax2.addActionListener(onAxisButtonPress);
-		
-		inc_ax3 = new JButton("+");
-		inc_ax3.addActionListener(onAxisButtonPress);
-		dec_ax3 = new JButton("-");
-		dec_ax3.addActionListener(onAxisButtonPress);
-		
+		incAx1 = new JButton( "+" );
+		incAx1.addActionListener( onAxisButtonPress );
+		decAx1 = new JButton( "-" );
+		decAx1.addActionListener( onAxisButtonPress );
+
+		incAx2 = new JButton( "+" );
+		incAx2.addActionListener( onAxisButtonPress );
+		decAx2 = new JButton( "-" );
+		decAx2.addActionListener( onAxisButtonPress );
+
+		incAx3 = new JButton( "+" );
+		incAx3.addActionListener( onAxisButtonPress );
+		decAx3 = new JButton( "-" );
+		decAx3.addActionListener( onAxisButtonPress );
+
 		/**
 		 * Create axis degree labels
 		 */
-		deg_ax1 = new JLabel("90");
-		deg_ax2 = new JLabel("0");
-		deg_ax3 = new JLabel("0");
-		
+		degAx1 = new JLabel( "" );
+		degAx2 = new JLabel( "" );
+		degAx3 = new JLabel( "" );
+
 		/**
 		 * Create control panel, add buttons
 		 */
 		JPanel controlPanel = new JPanel();
-		controlPanel.setBorder(new EmptyBorder(10,10,10,10));
-		controlPanel.setLayout(new GridBagLayout());
+		controlPanel.setBackground( Color.WHITE );
+		controlPanel.setBorder( new EmptyBorder( 10, 10, 10, 10 ) );
+		controlPanel.setLayout( new GridBagLayout() );
 		GridBagConstraints c = new GridBagConstraints();
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.gridx = 0;
 		c.gridy = 1;
-		controlPanel.add(new JLabel("Axis 1", SwingConstants.CENTER), c);
+		controlPanel.add( new JLabel( "Axis 1", SwingConstants.CENTER ), c );
 		c.gridx = 1;
 		c.gridy = 1;
-		controlPanel.add(inc_ax1, c);
+		controlPanel.add( incAx1, c );
 		c.gridx = 2;
 		c.gridy = 1;
-		controlPanel.add(deg_ax1, c);
+		controlPanel.add( degAx1, c );
 		c.gridx = 3;
 		c.gridy = 1;
-		controlPanel.add(dec_ax1, c);
-		
+		controlPanel.add( decAx1, c );
+
 		c.gridx = 0;
 		c.gridy = 2;
-		controlPanel.add(new JLabel("Axis 2", SwingConstants.CENTER), c);
+		controlPanel.add( new JLabel( "Axis 2", SwingConstants.CENTER ), c );
 		c.gridx = 1;
 		c.gridy = 2;
-		controlPanel.add(inc_ax2, c);
+		controlPanel.add( incAx2, c );
 		c.gridx = 2;
 		c.gridy = 2;
-		controlPanel.add(deg_ax2, c);
+		controlPanel.add( degAx2, c );
 		c.gridx = 3;
 		c.gridy = 2;
-		controlPanel.add(dec_ax2, c);
-		
+		controlPanel.add( decAx2, c );
+
 		c.gridx = 0;
 		c.gridy = 3;
-		controlPanel.add(new JLabel("Axis 2", SwingConstants.CENTER), c);
+		controlPanel.add( new JLabel( "Axis 2", SwingConstants.CENTER ), c );
 		c.gridx = 1;
 		c.gridy = 3;
-		controlPanel.add(inc_ax3, c);
+		controlPanel.add( incAx3, c );
 		c.gridx = 2;
 		c.gridy = 3;
-		controlPanel.add(deg_ax3, c);
+		controlPanel.add( degAx3, c );
 		c.gridx = 3;
 		c.gridy = 3;
-		controlPanel.add(dec_ax3, c);
-		
+		controlPanel.add( decAx3, c );
+
 		c.gridwidth = 4;
 		c.gridx = 0;
 		c.gridy = 0;
-		controlPanel.add(new JLabel("Control Panel", SwingConstants.CENTER), c);
-		
-		
-		
+		controlPanel.add( new JLabel( "Control Panel", SwingConstants.CENTER ),
+				c );
+
+		/**
+		 * Create the overall panel, and incorporate
+		 *   all the sub-panels for this project
+		 */
 		JPanel panes = new JPanel();
-		panes.setLayout(new BorderLayout());
-		panes.add(displayPanel, BorderLayout.CENTER);
-		panes.add(controlPanel, BorderLayout.EAST);
-		
-		
-		JFrame window = new JFrame("Team Ares - Project 1");
-		window.setContentPane(panes);
-		window.setSize(800, 600);
-		window.setLocation(100, 100);
-		window.setVisible(true);
+		panes.setLayout( new BorderLayout() );
+		panes.add( displayPanel, BorderLayout.CENTER );
+		panes.add( controlPanel, BorderLayout.EAST );
+
+		/**
+		 * Additional items needed to setup the window
+		 */
+		JFrame window = new JFrame( "Team Ares - Project 1" );
+		window.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		window.setContentPane( panes );
+		window.setSize( 830, 630 );
+		window.setLocation( 100, 100 );
+		window.setVisible( true );
+		window.setResizable( false );
 
 	}
 
---
+++ src/RoboticArm.java
@@ -0,0 +1,86 @@
+import java.awt.Graphics;
+import java.awt.Point;
+
+public class RoboticArm
+{
+	private RoboticLink link1, link2, link3;
+
+	public RoboticArm( int globalX, int globalY )
+	{
+		link1 = new RoboticLink( globalX, globalY, 150, 270 );
+		link2 = new RoboticLink( globalX, globalY, 100, 0 );
+		link3 = new RoboticLink( globalX, globalY, 75, 0 );
+
+		this.onTranslate();
+	}
+
+	public void incrementLink1()
+	{
+		link1.incrementAngle();
+	}
+
+	public void incrementLink2()
+	{
+		link2.incrementAngle();
+	}
+
+	public void incrementLink3()
+	{
+		link3.incrementAngle();
+	}
+
+	public void decrementLink1()
+	{
+		link1.decrementAngle();
+	}
+
+	public void decrementLink2()
+	{
+		link2.decrementAngle();
+	}
+
+	public void decrementLink3()
+	{
+		link3.decrementAngle();
+	}
+
+	public int getLink1Angle()
+	{
+		return link1.getAngle();
+	}
+
+	public int getLink2Angle()
+	{
+		return link2.getAngle();
+	}
+
+	public int getLink3Angle()
+	{
+		return link3.getAngle();
+	}
+
+	/**
+	 * Called after any values are changed for one of the robotic links
+	 */
+	public void onTranslate()
+	{
+		link1.setGlobalPosition( new Point( 300, 600 ), 0 );
+		link1.onTranslate();
+		link2.setGlobalPosition( link1.getEndPointGlobal(), link1.getAngle() );
+		link2.onTranslate();
+		link3.setGlobalPosition( link2.getEndPointGlobal(), link1.getAngle()
+				+ link2.getAngle() );
+		link3.onTranslate();
+	}
+
+	/**
+	 * Draws each of the robotic links
+	 * @param g Graphics
+	 */
+	public void onDraw( Graphics g )
+	{
+		link1.onDraw( g );
+		link2.onDraw( g );
+		link3.onDraw( g );
+	}
+}
